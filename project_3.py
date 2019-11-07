a92d43204b0792dda3ed2e4a7950b2af20a4119e
Jack Amos <jack671guam@yahoo.com>
2019-11-07 11:25:01 -0700

    Completed

--- project_3.py
+++ project_3.py
@@ -5,22 +5,19 @@
 #Python 3.7
 
 
-#operate on any nxn size matrix
-#read file as: dimension, values
-#sys matrix is nxn, prod matix is nx1
-#divide above value by below value to get quotient then mult lower row by quotient and sub upper row from lower row
-#make sure to perform operations on prod matrix as well
-#if x < .00001, x = 0
-
-
 
 def printSysProd(sys,prod,col_round):
 
 	count = len(sys)
 	row_string = ""
 
-	print("\nColumn %d"%(col_round))
+	#prints matrix heading
+	if col_round > 0:
+		print("\nColumn %d"%(col_round))
+	else:
+		print("\nSystem matrix:")
 
+	#prints matrix
 	while count != 0:
 
 		for n in sys:
@@ -38,7 +35,7 @@ def printSysProd(sys,prod,col_round):
 def sysCheck(sys):
 
 	num_zeros = 0
-	count = len(sys)
+	count = len(sys)-1
 
 	while count != 0:
 		num_zeros+=count
@@ -104,58 +101,130 @@ def solveForX(sys,prod):
 		end+=iterator
 
 
-	#check if all necessary 0's exist
+	#make necessary 0's
 	check = False
-	i = 0
-	col_top = 0
-	num_zeros = 0
-	zero_count = 0
-	index = 0
-	top = 0.0
 	col_round = 1
+	top = 0
+	top_value = 0.0
+	current = 0
+	current_value = 0.0
 
+	printSysProd(sysFinal,prod_values,0)
 
 	while check == False:
 
-		print(sysFinal)
 		#gaussian solving logic
-		for n in sysFinal:
+		top_value = sysFinal[top][top]
+		current = top + 1
 
-			num_zeros = size - 1
+		while current < len(sysFinal):
 
-			if n[i] == 0.0:
-				zero_count+=1
-			elif n != sysFinal[col_top] and n != sysFinal[0]:
+			current_value = sysFinal[current][top]
 
-				top = -1 * (sysFinal[col_top][i])
-				current = n[i]
+			if current_value != 0.0:
 
-				for m in n:
-					n[n.index(m)] = (m * top) + (sysFinal[col_top][n.index(m)] * current)
-					prod_values[sysFinal.index(n)] = (prod_values[sysFinal.index(n)] * top) + (prod_values[col_top] * current)
+				row_length = 0
+				prod_values[current] = (prod_values[current] * top_value * -1) + (prod_values[top] * current_value)
 
-				zero_count+=1
-		col_top+=1
-		i+=1
+				while row_length < len(sysFinal):
+					
+					sysFinal[current][row_length] = (sysFinal[current][row_length] * top_value * -1) + (sysFinal[top][row_length] * current_value)
+
+					row_length+=1
 
 
-		#set value to 0 if below or at minimum of .00001
+			current+=1
+		
+		top+=1
+
+
+		#set value to 0 if below or at minimum of .0000001, -.0000001, -0, and keeps values from becoming too small
 		for n in sysFinal:
 			for m in n:
 
-				if m <= .00001 and m > 0.0:
+				if m <= .0000001 and m > 0.0:
 					n[n.index(m)] = 0.0
-				elif m >= -.00001 and m < 0.0:
-					n[n.index(m)] = 0.0			
+				elif m >= -.0000001 and m < 0.0:
+					n[n.index(m)] = 0.0
+				elif m == -0.0:
+					n[n.index(m)] = 0.0
+
+		for n in prod_values:
+
+			if n <= .0000001 and n > 0.0:
+				n = 0.0
+			elif n >= -.0000001 and n < 0.0:
+				n = 0.0
+			elif n == -0.0:
+				n = 0.0
+
+
+		#reduces size of floating point values without removing them
+		big_count = 0
+		current_array = []
+
+		for n in sysFinal:
+
+			current_array = sysFinal[sysFinal.index(n)]
+			
+			if prod_values[sysFinal.index(n)] > 100 or prod_values[sysFinal.index(n)] < -100:
+
+				prod_values[sysFinal.index(n)] = prod_values[sysFinal.index(n)]/100
+			
+				while big_count < len(sysFinal):
+
+					current_array[big_count] = current_array[big_count]/100
+
+					big_count+=1
+
+			big_count = 0
 
 
 		printSysProd(sysFinal,prod_values,col_round)
 		col_round+=1
 
 		check = sysCheck(sysFinal)
-	print(check)
+
+	#solve for x values, starting at bottom
+	x_values = []
+	i = 0
+	
+	#make array of x's based on matrix size
+	while i < len(sysFinal):
+
+		x_values.append(1)
+		i+=1
+
+	i-=1
+	solveX = 0.0
+	f = len(sysFinal)-1
+	current_array = sysFinal[len(sysFinal)-1]
+	divVal = 0.0
+	
+	#gets sum of all current x's and row values then subtracts non important x positions and divides product value by important x
+	while f > -1:
+
+		while i > -1:
+			
+			if i != f:
+				solveX += x_values[i] * current_array[i]
+			elif i == f:
+				divVal = current_array[i]
+			i-=1
+
+		x_values[f] = (prod_values[f] - solveX)/divVal
+		f-=1
+		i = len(sysFinal)-1
+		solveX = 0.0
+		current_array = sysFinal[f]
 
 
+	#print x values
+	print("\nSolution Vector:")
+
+	for n in x_values:
+
+		print("%.4f"%n)
 
 
 
